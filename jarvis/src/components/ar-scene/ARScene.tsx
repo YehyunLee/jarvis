2c192566a3b3269ba61e2768ccb1a095667825e5
YehyunLee <glad7cu@gmail.com>
2025-06-22 06:03:57 -0400

    scroll automatic

--- jarvis/src/components/ar-scene/ARScene.tsx
+++ jarvis/src/components/ar-scene/ARScene.tsx
@@ -29,6 +29,7 @@ const ARWindow = class {
   title: string;
   position: { x: number; y: number; z: number };
   cssObject: CSS3DObject | null = null;
+  scrollInterval: number | null = null;
 
   constructor(id: string, scene: THREE.Scene, options: any = {}) {
     this.id = id;
@@ -123,6 +124,16 @@ const ARWindow = class {
     // contentDiv.style.whiteSpace = 'pre';
     contentDiv.innerHTML = htmlContent;
     container.appendChild(contentDiv);
+    // Auto-scroll HTML content (loop back when reaching bottom)
+    if (this.scrollInterval) clearInterval(this.scrollInterval);
+    this.scrollInterval = window.setInterval(() => {
+      const maxScroll = contentDiv.scrollHeight - contentDiv.clientHeight;
+      if (contentDiv.scrollTop >= maxScroll) {
+        contentDiv.scrollTop = 0;
+      } else {
+        contentDiv.scrollTop += 1;
+      }
+    }, 50);
     const cssObj = new CSS3DObject(container);
     // Position to align with content plane
     cssObj.position.copy(this.contentMesh!.position);
@@ -249,6 +260,8 @@ const ARWindow = class {
   }
 
   destroy() {
+    // Clear auto-scroll interval
+    if (this.scrollInterval) clearInterval(this.scrollInterval);
     if (this.group.parent) {
       this.group.parent.remove(this.group);
     }
